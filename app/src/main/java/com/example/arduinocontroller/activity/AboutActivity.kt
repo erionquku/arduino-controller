e435df750b7600b51bca8eb7ace75958fa6daf42
Erion <eriquku@gmail.com>
2021-05-11 22:31:15 +0200

    Added About Screen

---
+++ app/src/main/java/com/example/arduinocontroller/activity/AboutActivity.kt
@@ -0,0 +1,12 @@
+package com.example.arduinocontroller.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.arduinocontroller.R
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
